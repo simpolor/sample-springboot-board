2f227f7903291de81747764ff6707044b7dcd94a
sy.park <sy.park@powderroom.co.kr>
2023-02-08 16:21:19 +0900

    .

--- toast-editor/src/main/java/io/simpolor/toasteditor/controller/FileController.java
+++ toast-editor/src/main/java/io/simpolor/toasteditor/controller/FileController.java
@@ -2,11 +2,13 @@ package io.simpolor.toasteditor.controller;
 
 import com.google.gson.JsonObject;
 import io.simpolor.toasteditor.component.FileUploadComponent;
-import io.simpolor.toasteditor.model.FileMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
@@ -21,7 +23,7 @@ public class FileController {
 	@ResponseBody
 	public JsonObject upload(@RequestParam("image") MultipartFile multipartFile) {
 
-		FileMessage fileMessage = fileUploadComponent.upload(multipartFile);
+		FileUploadComponent.FileUpload fileMessage = fileUploadComponent.upload(multipartFile);
 		if(Boolean.TRUE.equals(fileMessage.getResult())){
 			JsonObject jsonObject = new JsonObject();
 			jsonObject.addProperty("imageUrl", "/upload/"+fileMessage.getSavedFileName());
